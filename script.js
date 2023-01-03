596489892f4f6385c68418b675bdb8ef684c9bf3
Patryk Lachowicz <118370365+riftmat@users.noreply.github.com>
2023-01-03 14:44:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let spec = document.getElementById("!@#");
 let btn = document.getElementById("done");
 let result = document.getElementById("resu");
 
+    //each input random generator
 function RandomSmall() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
@@ -22,6 +23,7 @@ function RandomSymbol() {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+    //btn gets password outcome
 btn.addEventListener("click", () => {
     const length = passLength.value;
     const numbers = num.checked;
@@ -30,6 +32,10 @@ btn.addEventListener("click", () => {
     result.value = generatePassword(numbers, symbols, size, length);
 });
 
+    //loop that checks if inputs are checked and generates password acording to selected options
+    /* i = -100 because if i = 0 and non of the inputs are checked it only generates 2 values.
+    Max user input is set to 50 so double the amount of values is enough to meet max and min user input
+    */
 function generatePassword(number, symbol, size, length) {
     let generatedPassword = "";
     let variations = [number, symbol, size].length;
@@ -50,3 +56,22 @@ function generatePassword(number, symbol, size, length) {
     return finalPassword;
 };
 
+    // button and checkbox animation
+
+const play = [
+    { transform: "scale(1)"},
+    { transform: "scale(0.95)"},
+    { transform: "scale(0.90)"},
+    { transform: "scale(0.95)"},
+    { transform: "scale(1)"},
+];
+
+const playtime = {
+    duration: 150,
+}
+
+document.querySelectorAll(".big input, .number input, .special input, .done input").forEach(item => {
+    item.addEventListener("click", () => {
+        item.animate(play, playtime);
+    })
+});
